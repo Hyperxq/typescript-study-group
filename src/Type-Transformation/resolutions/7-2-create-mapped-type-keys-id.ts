2ec6867524be187dc18c58b160c35f82f129c4cb
Hector Triana Lopez <hector.trianalopez@publicisgroupe.com>
2024-04-24 22:34:09 -0500

    feat: proposal solutions type transformations

---
+++ src/Type-Transformation/resolutions/7-2-create-mapped-type-keys-id.ts
@@ -0,0 +1,34 @@
+import { Equal, Expect } from "../../../helper";
+
+// 7.2 create a mapped type with onle the keys containing "id"
+
+interface Example {
+    name: string;
+    age: number;
+    id: string;
+    organisationId: string;
+    groupId: string;
+}
+
+/*
+  Checks if the key's value is of type string and if the key itself ends with "Id".
+  If both conditions are met, the key is included in the resulting mapped type; 
+  otherwise, it is excluded (never).
+*/
+type OnlyIdKeys<T> = {
+    [K in keyof T as K extends `${string}id` | `${string}Id` ? K : never]: T[K]
+};
+
+type tests = [
+    Expect<
+        Equal<
+            OnlyIdKeys<Example>,
+            {
+                id: string;
+                organisationId: string;
+                groupId: string;
+            }
+        >
+    >,
+    Expect<Equal<OnlyIdKeys<{}>, {}>>
+];
