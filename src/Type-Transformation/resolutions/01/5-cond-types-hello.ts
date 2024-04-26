969422eaa9c6a2fc44d08519870868042b1ce204
Hector Triana Lopez <hector.trianalopez@publicisgroupe.com>
2024-04-26 18:21:53 -0500

    fix: change last input string generics

--- src/Type-Transformation/resolutions/01/5-cond-types-hello.ts
+++ src/Type-Transformation/resolutions/01/5-cond-types-hello.ts
@@ -2,7 +2,7 @@ import { Equal, Expect } from "../../../../helper";
 
 // 5. conditional types, want to return goodbye if u pass hello, and vice versa
 
-type YouSayGoodbyeAndISayHello<T extends string | number> = T extends "hello"
+type YouSayGoodbyeAndISayHello<T extends string> = T extends "hello"
  ? "goodbye" 
  : T extends "goodbye"
  ? "hello"
@@ -18,5 +18,5 @@ type tests2 = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
   Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
   Expect<Equal<YouSayGoodbyeAndISayHello<"alright pal">, never>>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<1>, never>>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<'1'>, never>>,
 ];
